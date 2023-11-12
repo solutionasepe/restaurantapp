8c546d8365101f5269a407612003381481896531
solutionasepe <144459837+solutionasepe@users.noreply.github.com>
2023-11-12 15:54:47 +0100

    main

--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-config_path = os.path.join(BASE_DIR, '.env')
-config.read(config_path)
+# config_path = os.path.join(BASE_DIR, '.env')
+# config.read(config_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
