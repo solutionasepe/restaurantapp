76c31031ac41b820c064a4e6590233ab18cb33e2
solutionasepe <144459837+solutionasepe@users.noreply.github.com>
2023-11-12 15:37:38 +0100

    changed

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -111,14 +111,14 @@ if DEBUG:
             'ENGINE' : 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DATABASE_NAME'),
             'USER': config('DATABASE_USER'),
-            'PASSWORD': config("DATABASE_PASSWORD"),
+            'PASSWORD': config('DATABASE_PASSWORD'),
             'HOST': 'localhost',
             'PORT': '5432',
         }
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(config("DATABASE"))
+        'default': dj_database_url.parse(config('DATABASE'))
     }
 
 
